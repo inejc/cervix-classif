47c58c02edd6246d0efe8b57a871303c71fc6003
inejc <nejc.ilenic@gmail.com>
2017-04-08 12:20:36 +0200

    add latest xception results and submission

--- src/average_submissions.py
+++ src/average_submissions.py
@@ -7,9 +7,10 @@ from data_provider import SUBMISSIONS_DIR
 from utils import read_lines, create_submission_file
 
 W_SUBMISSIONS = {
-    'xception_fine_tuned_cleaned_0_72837.csv': 6,
-    'xception_fine_tuned_cleaned_0_73764.csv': 5,
-    'xception_fine_tuned_cleaned_0_74836.csv': 4,
+    'xception_fine_tuned_cleaned_0_72837.csv': 8,
+    'xception_fine_tuned_cleaned_0_73764.csv': 7,
+    'xception_fine_tuned_cleaned_0_73830.csv': 7,
+    'xception_fine_tuned_cleaned_0_74836.csv': 6,
     'xception_fine_tuned_0.76231.csv': 2,
     'xception_fine_tuned_0_77085.csv': 1,
     'xception_fine_tuned_cleaned_0_77236.csv': 1,
@@ -41,7 +42,10 @@ def average():
     averaged = probs * weights[:, np.newaxis, np.newaxis]
     averaged = np.sum(averaged, axis=0) / np.sum(weights)
 
-    submissions_file = join(SUBMISSIONS_DIR, 'averaged.csv')
+    submissions_file = join(
+        SUBMISSIONS_DIR,
+        'xception_fine_tuned_best_7_averaged.csv'
+    )
     create_submission_file(names, averaged, submissions_file)
 
 
