b10014d617b0b985f5ce098965debdad7c7f9cbb
Tim <ts0928@student.uni-lj.si>
2017-04-18 23:51:47 +0200

    push

--- src/train_head_on_bottlenecks.py
+++ src/train_head_on_bottlenecks.py
@@ -9,35 +9,32 @@ from keras.regularizers import l2
 
 from data_provider import MODELS_DIR
 
-WEIGHTS_PATH = join(MODELS_DIR, 'resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5')
-BOTTLENECKS_FILE = join(MODELS_DIR, "resnet50_bottlenecks.h5")
-LABELS_FILE = join(MODELS_DIR, "resnet50_labels.h5")
 
-MODEL_PATH = join(MODELS_DIR, "resnet50_head_model_on_bottlenecks.h5")
+def fit(epochs=1000, lr=0.001, dropout=0.5, l2_reg=0.05):
+    WEIGHTS_PATH = join(MODELS_DIR, 'resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5')
+    BOTTLENECKS_FILE = join(MODELS_DIR, "resnet50_bottlenecks.h5")
+    LABELS_FILE = join(MODELS_DIR, "resnet50_labels.h5")
 
-with h5py.File(BOTTLENECKS_FILE) as hf:
-    X_train = hf["train"][:]
-    X_valid = hf["valid"][:]
+    MODEL_PATH = join(MODELS_DIR, "resnet50_head_model_on_bottlenecks.h5")
 
-with h5py.File(LABELS_FILE) as hf:
-    y_train = hf["train"][:]
-    y_valid = hf["valid"][:]
+    with h5py.File(BOTTLENECKS_FILE) as hf:
+        X_train = hf["train"][:]
+        X_valid = hf["valid"][:]
 
-print(X_train.shape)
-print(X_valid.shape)
-print(y_train.shape)
-print(y_valid.shape)
+    with h5py.File(LABELS_FILE) as hf:
+        y_train = hf["train"][:]
+        y_valid = hf["valid"][:]
 
-cb = [ModelCheckpoint(MODEL_PATH, save_best_only=True)]
+    cb = [ModelCheckpoint(MODEL_PATH, save_best_only=True)]
 
-model = Sequential([
-    Dropout(0.6, input_shape=X_train.shape[1:]),
-    # Dense(8, init='uniform', activation='relu'),
-    # Dense(4, init='uniform', activation='relu'),
-    Dense(3, activation="softmax", W_regularizer=l2(0.05))
-])
+    model = Sequential([
+        Dropout(dropout, input_shape=X_train.shape[1:]),
+        # Dense(8, init='uniform', activation='relu'),
+        # Dense(4, init='uniform', activation='relu'),
+        Dense(3, activation="softmax", W_regularizer=l2(l2_reg))
+    ])
 
-model.compile(Adam(lr=0.001, decay=0.01), "categorical_crossentropy", metrics=["accuracy"])
-model.fit(X_train, y_train, callbacks=cb, validation_data=(X_valid, y_valid), epochs=100)
+    model.compile(Adam(lr=lr, decay=0.01), "categorical_crossentropy", metrics=["accuracy"])
+    model.fit(X_train, y_train, callbacks=cb, validation_data=(X_valid, y_valid), epochs=epochs)
 
-print("Finished fitting the model!")
+    print("Finished fitting the model!")
