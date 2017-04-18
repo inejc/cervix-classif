47999598f4fe01da3d3256ccb7da99501ae4ec55
inejc <nejc.ilenic@gmail.com>
2017-04-18 22:13:23 +0200

    stacking fix empty prediction arrays init

--- src/stacking.py
+++ src/stacking.py
@@ -29,7 +29,8 @@ MODELS = {
 def train(name='stable'):
     data_info = load_organized_data_info(imgs_dim=HEIGHT, name=name)
 
-    preds_val, preds_te = np.array([]), np.array([])
+    preds_val = np.empty((-1, 3 * len(MODELS)))
+    preds_te = np.empty((-1, 3 * len(MODELS)))
 
     for model_name, preprocess_func in MODELS.items():
         model_path = join(MODELS_DIR, model_name)
