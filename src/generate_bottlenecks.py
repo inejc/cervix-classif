c564bdcf8bfcfdaf9f3ad2792c8e65705735fa3e
Tim <ts0928@student.uni-lj.si>
2017-04-18 23:38:42 +0200

    push

--- src/generate_bottlenecks.py
+++ src/generate_bottlenecks.py
@@ -15,7 +15,7 @@ WEIGHTS_PATH = join(MODELS_DIR, 'resnet50_weights_tf_dim_ordering_tf_kernels_not
 BOTTLENECKS_FILE = join(MODELS_DIR, "resnet50_bottlenecks.h5")
 LABELS_FILE = join(MODELS_DIR, "resnet50_labels.h5")
 
-data_info = load_organized_data_info(imgs_dim=299, name="cropped")
+data_info = load_organized_data_info(imgs_dim=299, name="cropped_tsmole_path")
 
 dir_tr, num_tr = data_info['dir_tr'], data_info['num_tr']
 dir_val, num_val = data_info['dir_val'], data_info['num_val']
