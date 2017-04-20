8efc5856e12679b556b804e9d39efe8899d97539
inejc <nejc.ilenic@gmail.com>
2017-04-20 12:22:03 +0200

    fix latest model files

--- src/fix_model_files.py
+++ src/fix_model_files.py
@@ -6,7 +6,7 @@ from data_provider import MODELS_DIR
 
 MODELS = [
     'xception_fine_tuned_stable_frozen_106_dropout_0_5_val_loss_0_7550.h5',
-    'xception_fine_tuned_stable_frozen_96_dropout_0_3_val_loss_0_7326.h5',
+    'xception_fine_tuned_stable_frozen_96_dropout_0_5_val_loss_0_7359.h5',
     'xception_fine_tuned_stable_frozen_76_dropout_0_5_val_loss_0_7702.h5',
     'xception_fine_tuned_stable_frozen_66_dropout_0_5_val_loss_0_7719.h5',
 ]
