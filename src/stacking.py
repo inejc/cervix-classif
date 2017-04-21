0104e8a30683c2613fad6ac5bbd7c5b0599874dd
inejc <nejc.ilenic@gmail.com>
2017-04-21 21:15:10 +0200

    latest stacking

--- src/stacking.py
+++ src/stacking.py
@@ -24,29 +24,11 @@ BATCH_SIZE = 32
 MODELS = {
     'xception_fine_tuned_stable_frozen_86_dropout_0_2_val_loss_0_7288.h5':
         xception_preprocess,
-    'xception_fine_tuned_stable_frozen_86_dropout_0_5_val_loss_0_7520.h5':
-        xception_preprocess,
-    'xception_fine_tuned_stable_frozen_86_dropout_0_6_val_loss_0_7386.h5':
-        xception_preprocess,
-    'xception_fine_tuned_stable_frozen_96_dropout_0_6_val_loss_0_7383.h5':
-        xception_preprocess,
     'inception_fine_tuned_stable_frozen_280_dropout_0_5_val_loss_0_7203.h5':
         inception_preprocess,
-    'inception_fine_tuned_stable_frozen_260_dropout_0_5_val_loss_0_7440.h5':
-        inception_preprocess,
-    'inception_fine_tuned_stable_frozen_250_dropout_0_5_val_loss_0_7473.h5':
-        inception_preprocess,
-    'resnet50_fine_tuned_stable_frozen_150_dropout_0_5_val_loss_0_7410.h5':
-        resnet50_preprocess,
-    'resnet50_fine_tuned_stable_frozen_140_dropout_0_5_val_loss_0_7365.h5':
-        resnet50_preprocess,
-    'resnet50_fine_tuned_stable_frozen_130_dropout_0_5_val_loss_0_6868.h5':
-        resnet50_preprocess,
     'resnet50_fine_tuned_stable_frozen_120_dropout_0_5_val_loss_0_7174.h5':
         resnet50_preprocess,
     'vgg19_fine_tuned_stable_frozen_17_penultimate_256_dropout_0_5_val_loss_0_6631.h5':
-        vgg19_preprocess,
-    'vgg19_fine_tuned_stable_frozen_7_penultimate_256_dropout_0_5_val_loss_0_7142.h5':
         vgg19_preprocess,
 }
 
