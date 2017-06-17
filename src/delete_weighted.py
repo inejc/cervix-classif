6f5efe1e4c59bb32bd25d947d278f8fb6ad09608
inejc <nejc.ilenic@gmail.com>
2017-06-18 00:19:30 +0200

    remove weighted

--- src/delete_weighted.py
+++ src/delete_weighted.py
@@ -9,13 +9,13 @@ from data_provider import DATA_DIR
 def delete(soft=True):
     dir_ = join(DATA_DIR, 'train_299_final')
 
-    weighted = filter(lambda x: 'weighted' in x, listdir(join(dir_, 'Type_2')))
+    weighted = [x for x in filter(lambda x: 'weighted' in x, listdir(join(dir_, 'Type_2')))]
     for file in weighted[:140]:
         print(file)
         if not soft:
             remove(file)
 
-    weighted = filter(lambda x: 'weighted' in x, listdir(join(dir_, 'Type_3')))
+    weighted = [x for x in filter(lambda x: 'weighted' in x, listdir(join(dir_, 'Type_3')))]
     for file in weighted[:135]:
         print(file)
         if not soft:
