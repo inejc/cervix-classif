40c08a51ee38c2e8bbc11b315e9fbd4a18234461
inejc <nejc.ilenic@gmail.com>
2017-06-18 17:33:53 +0200

    xception final results, submission pipeline

--- src/submission_pipeline.py
+++ src/submission_pipeline.py
@@ -13,57 +13,112 @@ from utils import create_submission_file
 from vgg16_fine_tune import preprocess_single_input as vgg16_preprocess
 from vgg19_fine_tune import preprocess_single_input as vgg19_preprocess
 
+# GROUPS = [
+#     {
+#         'uid': 0,
+#         'name': 'stable',
+#         'width': 299,
+#         'height': 299,
+#         'models': [
+#             ('xception_fine_tuned_stable_frozen_86_dropout_0_2_val_loss_0_7288.h5', xception_preprocess),
+#             ('inception_fine_tuned_stable_frozen_280_dropout_0_5_val_loss_0_7203.h5', inception_preprocess),
+#             ('resnet50_fine_tuned_stable_frozen_120_dropout_0_5_val_loss_0_7174.h5', resnet50_preprocess),
+#             ('vgg19_fine_tuned_stable_frozen_17_penultimate_256_dropout_0_5_val_loss_0_6631.h5', vgg19_preprocess),
+#             ('vgg16_fine_tuned_stable_frozen_6_penultimate_256_dropout_0_5_val_loss_0_6863.h5', vgg16_preprocess),
+#         ]
+#     },
+#     {
+#         'uid': 1,
+#         'name': 'stable',
+#         'width': 299,
+#         'height': 299,
+#         'models': [
+#             ('xception_fine_tuned_stable_frozen_96_dropout_0_6_val_loss_0_7383.h5', xception_preprocess),
+#             ('inception_fine_tuned_stable_frozen_270_dropout_0_5_val_loss_0_7166.h5', inception_preprocess),
+#             ('resnet50_fine_tuned_stable_frozen_130_dropout_0_5_val_loss_0_6868.h5', resnet50_preprocess),
+#             ('vgg19_fine_tuned_stable_frozen_12_penultimate_512_dropout_0_5_val_loss_0_6995.h5', vgg19_preprocess),
+#             ('vgg16_fine_tuned_stable_frozen_11_penultimate_512_dropout_0_5_val_loss_0_7298.h5', vgg16_preprocess),
+#         ]
+#     },
+#     {
+#         'uid': 2,
+#         'name': 'stable',
+#         'width': 299,
+#         'height': 299,
+#         'models': [
+#             ('xception_fine_tuned_stable_frozen_86_dropout_0_6_val_loss_0_7386.h5', xception_preprocess),
+#             ('inception_fine_tuned_stable_frozen_260_dropout_0_5_val_loss_0_7440.h5', inception_preprocess),
+#             ('resnet50_fine_tuned_stable_frozen_140_dropout_0_5_val_loss_0_7365.h5', resnet50_preprocess),
+#             ('vgg19_fine_tuned_stable_frozen_7_penultimate_512_dropout_0_5_val_loss_0_6881.h5', vgg19_preprocess),
+#             ('vgg16_fine_tuned_stable_frozen_11_penultimate_256_dropout_0_5_val_loss_0_7105.h5', vgg16_preprocess),
+#         ]
+#     },
+#     {
+#         'uid': 3,
+#         'name': 'stable',
+#         'width': 299,
+#         'height': 299,
+#         'models': [
+#             ('xception_fine_tuned_stable_frozen_86_dropout_0_5_val_loss_0_7520.h5', xception_preprocess),
+#             ('inception_fine_tuned_stable_frozen_250_dropout_0_5_val_loss_0_7473.h5', inception_preprocess),
+#             ('resnet50_fine_tuned_stable_frozen_150_dropout_0_5_val_loss_0_7410.h5', resnet50_preprocess),
+#             ('vgg19_fine_tuned_stable_frozen_7_penultimate_256_dropout_0_5_val_loss_0_7142.h5', vgg19_preprocess),
+#             ('vgg16_fine_tuned_stable_frozen_6_penultimate_512_dropout_0_5_val_loss_0_7330.h5', vgg16_preprocess),
+#         ]
+#     },
+# ]
+
 GROUPS = [
     {
         'uid': 0,
-        'name': 'stable',
+        'name': 'final',
         'width': 299,
         'height': 299,
         'models': [
-            ('xception_fine_tuned_stable_frozen_86_dropout_0_2_val_loss_0_7288.h5', xception_preprocess),
-            ('inception_fine_tuned_stable_frozen_280_dropout_0_5_val_loss_0_7203.h5', inception_preprocess),
-            ('resnet50_fine_tuned_stable_frozen_120_dropout_0_5_val_loss_0_7174.h5', resnet50_preprocess),
-            ('vgg19_fine_tuned_stable_frozen_17_penultimate_256_dropout_0_5_val_loss_0_6631.h5', vgg19_preprocess),
-            ('vgg16_fine_tuned_stable_frozen_6_penultimate_256_dropout_0_5_val_loss_0_6863.h5', vgg16_preprocess),
+            ('xception_fine_tuned_final_frozen_86_dropout_0_2.h5', xception_preprocess),
+            ('inception_fine_tuned_final_frozen_280_dropout_0_5.h5', inception_preprocess),
+            ('resnet50_fine_tuned_final_frozen_120_dropout_0_5.h5', resnet50_preprocess),
+            ('vgg19_fine_tuned_final_frozen_17_penultimate_256_dropout_0_5.h5', vgg19_preprocess),
+            ('vgg16_fine_tuned_final_frozen_6_penultimate_256_dropout_0_5.h5', vgg16_preprocess),
         ]
     },
     {
         'uid': 1,
-        'name': 'stable',
+        'name': 'final',
         'width': 299,
         'height': 299,
         'models': [
-            ('xception_fine_tuned_stable_frozen_96_dropout_0_6_val_loss_0_7383.h5', xception_preprocess),
-            ('inception_fine_tuned_stable_frozen_270_dropout_0_5_val_loss_0_7166.h5', inception_preprocess),
-            ('resnet50_fine_tuned_stable_frozen_130_dropout_0_5_val_loss_0_6868.h5', resnet50_preprocess),
-            ('vgg19_fine_tuned_stable_frozen_12_penultimate_512_dropout_0_5_val_loss_0_6995.h5', vgg19_preprocess),
-            ('vgg16_fine_tuned_stable_frozen_11_penultimate_512_dropout_0_5_val_loss_0_7298.h5', vgg16_preprocess),
+            ('xception_fine_tuned_final_frozen_96_dropout_0_6.h5', xception_preprocess),
+            ('inception_fine_tuned_final_frozen_270_dropout_0_5.h5', inception_preprocess),
+            ('resnet50_fine_tuned_final_frozen_130_dropout_0_5.h5', resnet50_preprocess),
+            ('vgg19_fine_tuned_final_frozen_12_penultimate_512_dropout_0_5.h5', vgg19_preprocess),
+            ('vgg16_fine_tuned_final_frozen_11_penultimate_512_dropout_0_5.h5', vgg16_preprocess),
         ]
     },
     {
         'uid': 2,
-        'name': 'stable',
+        'name': 'final',
         'width': 299,
         'height': 299,
         'models': [
-            ('xception_fine_tuned_stable_frozen_86_dropout_0_6_val_loss_0_7386.h5', xception_preprocess),
-            ('inception_fine_tuned_stable_frozen_260_dropout_0_5_val_loss_0_7440.h5', inception_preprocess),
-            ('resnet50_fine_tuned_stable_frozen_140_dropout_0_5_val_loss_0_7365.h5', resnet50_preprocess),
-            ('vgg19_fine_tuned_stable_frozen_7_penultimate_512_dropout_0_5_val_loss_0_6881.h5', vgg19_preprocess),
-            ('vgg16_fine_tuned_stable_frozen_11_penultimate_256_dropout_0_5_val_loss_0_7105.h5', vgg16_preprocess),
+            ('xception_fine_tuned_final_frozen_86_dropout_0_6.h5', xception_preprocess),
+            ('inception_fine_tuned_final_frozen_260_dropout_0_5.h5', inception_preprocess),
+            ('resnet50_fine_tuned_final_frozen_140_dropout_0_5.h5', resnet50_preprocess),
+            ('vgg19_fine_tuned_final_frozen_7_penultimate_512_dropout_0_5.h5', vgg19_preprocess),
+            ('vgg16_fine_tuned_final_frozen_11_penultimate_256_dropout_0_5.h5', vgg16_preprocess),
         ]
     },
     {
         'uid': 3,
-        'name': 'stable',
+        'name': 'final',
         'width': 299,
         'height': 299,
         'models': [
-            ('xception_fine_tuned_stable_frozen_86_dropout_0_5_val_loss_0_7520.h5', xception_preprocess),
-            ('inception_fine_tuned_stable_frozen_250_dropout_0_5_val_loss_0_7473.h5', inception_preprocess),
-            ('resnet50_fine_tuned_stable_frozen_150_dropout_0_5_val_loss_0_7410.h5', resnet50_preprocess),
-            ('vgg19_fine_tuned_stable_frozen_7_penultimate_256_dropout_0_5_val_loss_0_7142.h5', vgg19_preprocess),
-            ('vgg16_fine_tuned_stable_frozen_6_penultimate_512_dropout_0_5_val_loss_0_7330.h5', vgg16_preprocess),
+            ('xception_fine_tuned_final_frozen_86_dropout_0_5.h5', xception_preprocess),
+            ('inception_fine_tuned_final_frozen_250_dropout_0_5.h5', inception_preprocess),
+            ('resnet50_fine_tuned_final_frozen_150_dropout_0_5.h5', resnet50_preprocess),
+            ('vgg19_fine_tuned_final_frozen_7_penultimate_256_dropout_0_5.h5', vgg19_preprocess),
+            ('vgg16_fine_tuned_final_frozen_6_penultimate_512_dropout_0_5.h5', vgg16_preprocess),
         ]
     },
 ]
@@ -75,7 +130,8 @@ def make_submission():
     for group in GROUPS:
         te_names, te_preds = stack(group)
 
-        submission_name = 'stacked_group_{:d}.csv'.format(group['uid'])
+        submission_name = 'stacked_group_{:d}_{:s}.csv'.format(
+            group['uid'], group['name'])
         submissions[submission_name] = 1
         submission_file = join(SUBMISSIONS_DIR, submission_name)
         create_submission_file(te_names, te_preds, submission_file)
