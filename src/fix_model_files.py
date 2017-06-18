80b0839f9877cd29c5a59cb7f9403ae95cf3ee5b
inejc <nejc.ilenic@gmail.com>
2017-06-18 18:12:02 +0200

    fix model files, inception final models

--- src/fix_model_files.py
+++ src/fix_model_files.py
@@ -5,12 +5,26 @@ import h5py
 from data_provider import MODELS_DIR
 
 MODELS = [
-    'vgg16_fine_tuned_stable_frozen_11_penultimate_256_dropout_0_5_val_loss_0_7105.h5',
-    'vgg16_fine_tuned_stable_frozen_6_penultimate_256_dropout_0_5_val_loss_0_6863.h5',
-    'vgg16_fine_tuned_stable_frozen_3_penultimate_256_dropout_0_5_val_loss_0_7231.h5',
-    'vgg16_fine_tuned_stable_frozen_11_penultimate_512_dropout_0_5_val_loss_0_7298.h5',
-    'vgg16_fine_tuned_stable_frozen_6_penultimate_512_dropout_0_5_val_loss_0_7330.h5',
-    'vgg16_fine_tuned_stable_frozen_3_penultimate_512_dropout_0_5_val_loss_0_7377.h5',
+    'xception_fine_tuned_final_frozen_86_dropout_0_2.h5',
+    'inception_fine_tuned_final_frozen_280_dropout_0_5.h5',
+    'resnet50_fine_tuned_final_frozen_120_dropout_0_5.h5',
+    'vgg19_fine_tuned_final_frozen_17_penultimate_256_dropout_0_5.h5',
+    'vgg16_fine_tuned_final_frozen_6_penultimate_256_dropout_0_5.h5',
+    'xception_fine_tuned_final_frozen_96_dropout_0_6.h5',
+    'inception_fine_tuned_final_frozen_270_dropout_0_5.h5',
+    'resnet50_fine_tuned_final_frozen_130_dropout_0_5.h5',
+    'vgg19_fine_tuned_final_frozen_12_penultimate_512_dropout_0_5.h5',
+    'vgg16_fine_tuned_final_frozen_11_penultimate_512_dropout_0_5.h5',
+    'xception_fine_tuned_final_frozen_86_dropout_0_6.h5',
+    'inception_fine_tuned_final_frozen_260_dropout_0_5.h5',
+    'resnet50_fine_tuned_final_frozen_140_dropout_0_5.h5',
+    'vgg19_fine_tuned_final_frozen_7_penultimate_512_dropout_0_5.h5',
+    'vgg16_fine_tuned_final_frozen_11_penultimate_256_dropout_0_5.h5',
+    'xception_fine_tuned_final_frozen_86_dropout_0_5.h5',
+    'inception_fine_tuned_final_frozen_250_dropout_0_5.h5',
+    'resnet50_fine_tuned_final_frozen_150_dropout_0_5.h5',
+    'vgg19_fine_tuned_final_frozen_7_penultimate_256_dropout_0_5.h5',
+    'vgg16_fine_tuned_final_frozen_6_penultimate_512_dropout_0_5.h5',
 ]
 
 for model in MODELS:
